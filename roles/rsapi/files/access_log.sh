ba92b5883252ef2d9a43e58e9e970163298b1f27
Peter Storch <peter.storch@tuta.io>
2022-08-27 12:49:30 +0200

    Add access_log.sh

---
+++ roles/rsapi/files/access_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat /var/log/apache2/fotouebersicht_access.log /var/log/apache2/fotouebersicht_access.log.1 > /tmp/api.log
+for f in /var/log/apache2/fotouebersicht_access.log.*.gz ; do gunzip -c "$f" >> /tmp/api.log ; done
+
+sed -i 's/.*"\(.*\) HTTP\/.*/\1/g' /tmp/api.log
+sed -i 's/\/..\//\/*\//g' /tmp/api.log
+sed -i 's/?.*//g' /tmp/api.log
+sed -i 's/\/Z\?[0-9].*/\/*/g' /tmp/api.log
+sed -i 's/\/[^\/].*.jpg/\/*/g' /tmp/api.log
+sed -i 's/\/[^\/].*.png/\/*/g' /tmp/api.log
+sed -i 's/Verification\/.*/Verification\/*/g' /tmp/api.log
+sort -u --version-sort /tmp/api.log
